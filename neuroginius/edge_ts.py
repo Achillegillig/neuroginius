b033883bc46a85e12d5a8594024709c97e64f60f
Achille Gillig <achillegillig@hotmail.fr>
2024-05-16 12:15:17 +0200

    labeling of edge timeseries

--- neuroginius/edge_ts.py
+++ neuroginius/edge_ts.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.spatial.distance import squareform
 from scipy.stats import zscore
+from itertools import product
+import pandas as pd
 
 def compute_edge_timeseries(rsData, matrix_form=False):
     
@@ -15,21 +17,30 @@ def compute_edge_timeseries(rsData, matrix_form=False):
     else:
         indices_uppertriangle = [np.triu_indices(pairwise_products[0].shape[0], 1) for i in range(pairwise_products.shape[0])]
         edge_ts = np.array([prod[ind] for prod, ind in zip(pairwise_products, indices_uppertriangle)])
-    
-<<<<<<< HEAD
     return edge_ts
 
 def label_ets(labels):
     """
     labels: name of ROIs, size n_ROIs
-    return: ets_labels, size nROIs x nROIs 
+    return: ets_labels
     """
+    indices = np.triu_indices(len(labels), 1)
 
-    pairwise_products = np.outer(labels, labels)
-    indices = np.triu_indices(pairwise_products[0].shape[0], 1)
-    ts_labels = np.array(pairwise_products[indices])
+    ets_labels = []
+    for i, (x,y) in enumerate(zip(*indices)):
+        ets_labels.append((labels[x], labels[y]))
 
     return ets_labels
-=======
-    return edge_ts
->>>>>>> 962701e0569ba55708fd18965a7a85b52b60a1a3
+
+def ets_labels_to_int(ets_labels):
+    # Normalize each label by sorting the tuples
+    normalized_labels = [tuple(sorted(label)) for label in ets_labels]
+    
+    # Create a list of unique labels using a set to remove duplicates, then convert it back to a list and sort it
+    unique_labels = sorted(list(set(normalized_labels)))
+    
+    # Create an array of integer labels where each label corresponds to the index of the tuple in unique_labels plus one
+    int_labels = np.array([unique_labels.index(label) + 1 for label in normalized_labels], dtype=int)
+    
+    return int_labels
+        
